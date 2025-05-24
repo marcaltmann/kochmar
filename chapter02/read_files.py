b5326ee8f7783d0ea00d833fa61bf499439e90ed
Marc Altmann <marcaltmann@posteo.de>
2025-05-25 01:09:44 +0200

    WIP

--- chapter02/read_files.py
+++ chapter02/read_files.py
@@ -1,5 +1,15 @@
-import os
 import codecs
+import os
+import random
+
+import nltk
+from nltk import word_tokenize
+
+nltk.download("punkt")
+
+
+def tokenize(input: str) -> list[str]:
+    return word_tokenize(input)
 
 
 def read_text_files(directory: str) -> list[str]:
@@ -14,3 +24,18 @@ def read_text_files(directory: str) -> list[str]:
             f.close()
 
     return text_list
+
+
+def read_and_sort():
+    spam_list = read_text_files("enron1/spam/")
+    ham_list = read_text_files("enron1/ham/")
+    all_emails = [(email_content, "spam") for email_content in spam_list]
+    all_emails += [(email_content, "ham") for email_content in ham_list]
+
+    random.seed(42)
+    random.shuffle(all_emails)
+    print(f"Dataset size = {len(all_emails)} emails")
+
+
+if __name__ == "__main__":
+    read_and_sort()
